0e60dca97a78d8f66dd59bbc62ff8d617334288d
blairMe <felixanachoni@gmail.com>
2023-01-15 14:36:35 +0300

    Screens

--- app/src/main/java/com/gdsc_uoe/lostnfound/navigation/LostnFoundNavigation.kt
+++ app/src/main/java/com/gdsc_uoe/lostnfound/navigation/LostnFoundNavigation.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.gdsc_uoe.lostnfound.screens.chatsScreen.ChatsScreen
+import com.gdsc_uoe.lostnfound.screens.postScreen.PostScreen
+import com.gdsc_uoe.lostnfound.screens.profileScreen.ProfileScreen
 import com.gdsc_uoe.lostnfound.screens.searchScreen.SearchScreen
 import com.gdsc_uoe.lostnfound.screens.splash.SplashScreen
 
@@ -18,6 +21,15 @@ fun LostnFoundNavigation() {
         }
         composable(LostnFoundScreens.SearchScreen.name) {
             SearchScreen(navController = navController)
+        }
+        composable(LostnFoundScreens.PostScreen.name) {
+            PostScreen(navController = navController)
+        }
+        composable(LostnFoundScreens.ChatsScreen.name) {
+            ChatsScreen(navController = navController)
+        }
+        composable(LostnFoundScreens.ProfileScreen.name) {
+            ProfileScreen(navController = navController)
         }
     }
 
---
+++ app/src/main/java/com/gdsc_uoe/lostnfound/screens/postScreen/PostScreen.kt
@@ -0,0 +1,10 @@
+package com.gdsc_uoe.lostnfound.screens.postScreen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun PostScreen(navController: NavController) {
+    Text(text = "Post Screen")
+}
